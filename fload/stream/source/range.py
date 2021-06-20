4451f6592307e96ce88357a5745bdcfbeb093d03
KevenLi <pbleester@gmail.com>
2021-06-20 20:05:06 +0800

    range --step parameters

--- fload/stream/source/range.py
+++ fload/stream/source/range.py
@@ -5,17 +5,20 @@ class RangeSource(Source):
     field = 'x'
     _start = 1
     stop = 1
+    step = 1
 
     def add_arguments(self, parser):
-        parser.add_argument('field')
-        parser.add_argument('--start', type=int, default=1)
+        parser.add_argument('--field', default='x')
+        parser.add_argument('--start', type=int, default=0)
         parser.add_argument('--stop', type=int, default=1)
+        parser.add_argument('--step', type=int, default=1)
 
     def init(self, ops):
         self.field = ops.field
         self._start = ops.start
         self.stop = ops.stop
+        self.step = ops.step
 
     def start(self):
-        for i in range(self._start, self.stop + 1):
+        for i in range(self._start, self.stop, self.step):
             yield {self.field: i}
